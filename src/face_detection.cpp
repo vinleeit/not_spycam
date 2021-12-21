b96a0e3947e68040d8bc1983fff31bf6ca0c2e18
Arvin <gnimoaix857@gmail.com>
2021-12-22 01:36:37 +0700

    Added face recognition
    Added face recognition without local server.

--- src/face_detection.cpp
+++ src/face_detection.cpp
@@ -1,18 +1,82 @@
 #include <Arduino.h>
 #include "fd_forward.h"
+#include "fr_forward.h"
 #include "esp_camera.h"
 
 #define CAMERA_MODEL_AI_THINKER
 #include "camera_pins.h"
+#define ENROLL_CONFIRM_TIMES 5
+#define FACE_ID_SAVE_NUMBER 7
+#define BUTTON 13
 
-mtmn_config_t mtmn_config = {0};
-int detections = 0;
+static mtmn_config_t mtmnConfig = {0};
+static int detections = 0;
+static bool isRecognitionEnabled = true;
+static int8_t isEnrolling = 0;
+static face_id_list idList = {0};
+
+static int runFaceRecognition(dl_matrix3du_t *imageMatrix, box_array_t *boxes)
+{
+    dl_matrix3du_t *alignedFace = NULL;
+    int matchedID = 0;
+
+    alignedFace = dl_matrix3du_alloc(1, 56, 56, 3);
+    if (alignedFace == NULL)
+    {
+        Serial.println("Could not allocate face recognition buffer!");
+        return matchedID;
+    }
+    if (align_face(boxes, imageMatrix, alignedFace) == ESP_OK)
+    {
+        if (isEnrolling == 1)
+        {
+            int8_t leftSampleFace = enroll_face(&idList, alignedFace);
+
+            if (leftSampleFace == (ENROLL_CONFIRM_TIMES - 1))
+            {
+                Serial.printf("Enrolling Face ID: %d\n", idList.tail);
+            }
+            Serial.printf("Enrolling Face ID: %d sample %d\n", idList.tail, ENROLL_CONFIRM_TIMES - leftSampleFace);
+
+            // rgb_printf(imageMatrix, FACE_COLOR_SYAN, "ID[%u] Sample [%u]", idList.tail, ENROLL_CONFIRM_TIMES - leftSampleFace);
+            if (leftSampleFace == 0)
+            {
+                isEnrolling = 0;
+                Serial.printf("Enrolled Face ID: %d\n", idList.tail);
+            }
+        }
+        else
+        {
+            matchedID = recognize_face(&idList, alignedFace);
+            if (matchedID >= 0)
+            {
+                Serial.printf("Match Face ID: %u\n", matchedID);
+                // rgb_printf(imageMatrix, FACE_COLOR_GREEN, "Hello Subject %u", matchedID);
+            }
+            else
+            {
+                Serial.println("No Match Found");
+                // rgb_print(imageMatrix, FACE_COLOR_RED, "Intruder Alert!");
+                matchedID = -1;
+            }
+        }
+    }
+    else
+    {
+        Serial.println("Face Not Aligned");
+        // rgb_print(image_matrix, FACE_COLOR_YELLOW, "Human Detected");
+    }
+    dl_matrix3du_free(alignedFace);
+    return matchedID;
+}
 
 // Setup
 void setup()
 {
     Serial.begin(115200);
     Serial.setDebugOutput(true);
+    pinMode(BUTTON, INPUT);
+
     Serial.println("===<Setup>===");
 
     // Config
@@ -89,38 +153,63 @@ void setup()
 
     // MTMN
     Serial.print("# mtmn: ");
-    mtmn_config = mtmn_init_config();
-    mtmn_config.type = FAST;
-    mtmn_config.min_face = 80;
-    mtmn_config.pyramid = 0.707;
-    mtmn_config.pyramid_times = 4;
-    mtmn_config.p_threshold.score = 0.6;
-    mtmn_config.p_threshold.nms = 0.7;
-    mtmn_config.p_threshold.candidate_number = 20;
-    mtmn_config.r_threshold.score = 0.7;
-    mtmn_config.r_threshold.nms = 0.7;
-    mtmn_config.r_threshold.candidate_number = 10;
-    mtmn_config.o_threshold.score = 0.7;
-    mtmn_config.o_threshold.nms = 0.7;
-    mtmn_config.o_threshold.candidate_number = 1;
+    mtmnConfig = mtmn_init_config();
+    mtmnConfig.type = FAST;
+    mtmnConfig.min_face = 80;
+    mtmnConfig.pyramid = 0.707;
+    mtmnConfig.pyramid_times = 4;
+    mtmnConfig.p_threshold.score = 0.6;
+    mtmnConfig.p_threshold.nms = 0.7;
+    mtmnConfig.p_threshold.candidate_number = 20;
+    mtmnConfig.r_threshold.score = 0.7;
+    mtmnConfig.r_threshold.nms = 0.7;
+    mtmnConfig.r_threshold.candidate_number = 10;
+    mtmnConfig.o_threshold.score = 0.7;
+    mtmnConfig.o_threshold.nms = 0.7;
+    mtmnConfig.o_threshold.candidate_number = 1;
+    Serial.println("successful!");
+
+    // Face ID init
+    Serial.println("face id init: ");
+    face_id_init(&idList, FACE_ID_SAVE_NUMBER, ENROLL_CONFIRM_TIMES);
     Serial.println("successful!");
     Serial.println("===<Setup Complete>===");
 }
 
 void loop()
 {
+    if (digitalRead(BUTTON) == LOW)
+    {
+        if (isEnrolling == 0)
+        {
+            Serial.println("Enrolling!");
+            isEnrolling = 1;
+        }
+        else
+        {
+            Serial.println("Enrolling canceled");
+            isEnrolling = 0;
+        }
+    }
+
     camera_fb_t *frame;
     frame = esp_camera_fb_get();
+    int faceID = 0;
 
-    dl_matrix3du_t *image_matrix = dl_matrix3du_alloc(1, frame->width, frame->height, 3);
-    fmt2rgb888(frame->buf, frame->len, frame->format, image_matrix->item);
+    dl_matrix3du_t *imageMatrix = dl_matrix3du_alloc(1, frame->width, frame->height, 3);
+    fmt2rgb888(frame->buf, frame->len, frame->format, imageMatrix->item);
 
     esp_camera_fb_return(frame);
 
-    box_array_t *boxes = face_detect(image_matrix, &mtmn_config);
+    box_array_t *boxes = face_detect(imageMatrix, &mtmnConfig);
 
     if (boxes != NULL)
     {
+        if (isRecognitionEnabled)
+        {
+            faceID = runFaceRecognition(imageMatrix, boxes);
+        }
+
         detections += 1;
         Serial.printf("Faces detected %d times!\n", detections);
         dl_lib_free(boxes->score);
@@ -128,5 +217,5 @@ void loop()
         dl_lib_free(boxes->landmark);
         dl_lib_free(boxes);
     }
-    dl_matrix3du_free(image_matrix);
+    dl_matrix3du_free(imageMatrix);
 }
